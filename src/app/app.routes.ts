6cb0b2389ba5b97b2a48137fc7432d2442f01c45
David Reyes <david@MacBook-Air-de-David-243.local>
2026-01-14 00:30:48 -0600

    Add Host to microfrontend

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
+import { loadRemoteModule } from '@angular-architects/native-federation';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: 'wallet-view',
+    loadChildren: () =>
+      loadRemoteModule({
+        //remoteEntry: 'http://localhost:4201/remoteEntry.json',
+        remoteName: 'walletApp',
+        exposedModule: './routes' // Name of the exposed module in the remote
+      }).then(m => m.routes) // Assuming remote exposes a 'remoteRoutes' array
+  },];
